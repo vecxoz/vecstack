1627a7d8bdc3e96b2dc531aef6842173e341ff63
vecxoz <vecxoz@gmail.com>
2016-11-10 20:43:57 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
 #! /usr/bin/env python
+
+from setuptools import setup
+
+setup(name='vecstack',
+      version='0.1',
+      description='Python package for stacking',
+      url='https://github.com/vecxoz/vecstack',
+      author='vecxoz',
+      author_email='vecxoz@gmail.com',
+      license='GPL',
+      packages=['vecstack'],
+      zip_safe=False)
